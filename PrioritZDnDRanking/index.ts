1019354220316794b1827dba646dc7646a56a755
snizar00 <48803721+snizar00@users.noreply.github.com>
2023-04-13 15:00:18 -0700

    Update index.ts

--- PrioritZDnDRanking/index.ts
+++ PrioritZDnDRanking/index.ts
@@ -1,13 +1,11 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
-//import { HelloWorld, IHelloWorldProps } from "./HelloWorld";
-import { PriorityComponent, PriorityComponentProps } from "./PriorityComponent";
-import * as React from "react";     
+import { PriorityComponent, PriorityComponentProps } from './PriorityComponent';
+import * as React from "react";
 
-export class PrioritiZDnDRanking implements ComponentFramework.ReactControl<IInputs, IOutputs> {
-    
+export class PrioritZDnDRanking implements ComponentFramework.ReactControl<IInputs, IOutputs> {
     private context: ComponentFramework.Context<IInputs>;
     private items: ComponentFramework.PropertyTypes.DataSet;
-    
+
     private theComponent: ComponentFramework.ReactControl<IInputs, IOutputs>;
     private notifyOutputChanged: () => void;
 
@@ -23,14 +21,10 @@ export class PrioritiZDnDRanking implements ComponentFramework.ReactControl<IInp
      * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
      * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
      */
-    public i        nit(
-        context: ComponentFramework.Context<IInputs>,
-        notifyOutputChanged: () => void,
-        state: ComponentFramework.Dictionary
-    ): void {
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary): void {
         this.context = context;
         context.mode.trackContainerResize(true);
-                
+
         this.notifyOutputChanged = notifyOutputChanged;
     }
 
@@ -39,19 +33,28 @@ export class PrioritiZDnDRanking implements ComponentFramework.ReactControl<IInp
      * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
      * @returns ReactElement root react element for the control
      */
-    public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
-        const props: IHelloWorldProps = { name: 'Hello, World!' };
-        return React.createElement(
-            HelloWorld, props
-        );
+     public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
+        const dataset = context.parameters.items;
+        return React.createElement(PriorityComponent, {
+            width: context.mode.allocatedWidth,
+            height: context.mode.allocatedHeight,
+            itemHeight: context.parameters.ItemHeight.raw,
+            fontSize: context.parameters.FontSize.raw,
+            fontColor: context.parameters.FontColor.raw,
+            dataset: dataset,
+            onReorder: this.onReorder,
+            backgroundColor: this.context.parameters.BackgroundColor.raw,
+            dragBackgroundColor: this.context.parameters.DragBackgroundColor.raw,
+        } as PriorityComponentProps);
     }
+
 
     /**
      * It is called by the framework prior to a control receiving new data.
      * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
      */
     public getOutputs(): IOutputs {
-        return { };
+        return {};
     }
 
     /**
@@ -61,4 +64,15 @@ export class PrioritiZDnDRanking implements ComponentFramework.ReactControl<IInp
     public destroy(): void {
         // Add code to cleanup control if necessary
     }
+
+    onReorder = (sourceIndex: number, destinationIndex: number): void => {
+        const dataset = this.context.parameters.items;
+        const sourceId = dataset.sortedRecordIds[sourceIndex];
+        const destinationId = dataset.sortedRecordIds[destinationIndex];
+        // raise the OnSelect event
+        this.context.parameters.items.openDatasetItem(dataset.records[sourceId].getNamedReference());
+        // set the SelectedItems property
+        this.context.parameters.items.setSelectedRecordIds([sourceId, destinationId]);
+    };
+
 }
